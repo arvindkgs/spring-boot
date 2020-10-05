0efb5a710e8312521cc4d954365a397bf17b4273
Arvind Kumar GS <mail.arvind85@gmail.com>
2020-10-05 07:33:58 +0530

    Revert changes to Person

--- spring-boot-samples/spring-boot-sample-flyway/src/main/java/sample/flyway/Person.java
+++ spring-boot-samples/spring-boot-sample-flyway/src/main/java/sample/flyway/Person.java
@@ -18,13 +18,11 @@ package sample.flyway;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Person {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String firstName;
 	private String lastName;
